f4191e4d66d37984570706147d17bed332f56dde
Stanislav <stasvitalich1994@gmail.com>
2022-09-06 00:11:02 +0700

    The new folder has been created

--- Example2/Program.cs
+++ Example2/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Нахождение максимума среди целых чисел.
+//Решение задачи с использованием функции и массива.
+
+int Max(int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if(arg2 > result) result = arg2;
+    if(arg3 > result) result = arg3;
+    return result;
+}
+
+int[] array = { 11, 21, 31, 41, 15, 61, 17, 18, 19 };
+array[0] = 12;
+
+int result = Max(
+    Max(array[0], array[1], array [2]),
+    Max(array[3], array[4], array [5]),
+    Max(array[6], array[7], array [8])
+);
+Console.WriteLine(result);
+
+
+
