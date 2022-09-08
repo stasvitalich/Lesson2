521b86e74299074c57d241acbe7acde747d88e8f
Stanislav <stasvitalich1994@gmail.com>
2022-09-08 15:16:27 +0700

    1 and 2 Void has been added

--- Example4/Program4.cs
+++ Example4/Program4.cs
@@ -1,1 +1,27 @@
-﻿Console.WriteLine("Hello, World!");
+﻿void FillArray (int [] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection [index] = new Random().Next(1, 10);
+        index = index + 1;
+
+    }
+}
+
+void PrintArray (int [] col)
+
+{
+    int count = col.Length;
+    int position = 0;
+    while(position < count)
+    {
+        Console.WriteLine(col[position]);
+        position = position + 1;
+
+    }
+}
+
+
+int [] array = new int [10];
