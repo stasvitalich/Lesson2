31d0f88f812001222c95131bb30d8ae8ddc67408
Stanislav <stasvitalich1994@gmail.com>
2022-09-08 14:57:15 +0700

    The third program has been finished

--- Example3/Program.cs
+++ Example3/Program.cs
@@ -1,5 +1,18 @@
 ﻿//Поиск позиции нужного элемента
 
-int [] array = {1, 45, 12, 33, 6, 88, 64, 97};
+int [] array = {1, 12, 31, 4, 15, 18, 16, 17, 18};
 
+int n = array.Length;
+int find = 18;
+int index = 0;
+
+while(index < n)
+{   
+    if (array [index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index = index + 1;
+}
 
